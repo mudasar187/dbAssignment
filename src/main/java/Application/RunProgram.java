303e22cdef1f8fc4c6a1607e9904c420c53e4374
mudasar187 <ahmmud16@hotmail.com>
2017-11-27 19:09:49 +0100

    update

--- src/main/java/Application/RunProgram.java
+++ src/main/java/Application/RunProgram.java
@@ -23,6 +23,7 @@ public class RunProgram {
     public static void main(String[] args) throws InterruptedException {
 
         Program program = new Program();
+
         program.runProgram();
     }
 }
